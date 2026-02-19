9390961305f343aa176b6261cf88a596702f3c78
zainalsalamun <zainalrtf@gmail.com>
2026-02-19 16:20:02 +0700

    feat: implement shift management views and integrate shift selection into user forms and listings.

---
+++ resources/views/pages/shifts/edit.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('title', 'Shifts')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-daterangepicker/daterangepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/select2/dist/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/selectric/public/selectric.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-timepicker/css/bootstrap-timepicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-tagsinput/dist/bootstrap-tagsinput.css') }}">
+@endpush
+
+@section('main')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>Advanced Forms</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                    <div class="breadcrumb-item"><a href="#">Forms</a></div>
+                    <div class="breadcrumb-item">Shifts</div>
+                </div>
+            </div>
+
+            <div class="section-body">
+                <h2 class="section-title">Shifts</h2>
+                <p class="section-lead">We provide advanced input fields, such as date picker, color picker, and so on.</p>
+
+                <div class="card">
+                    <form action="{{ route('shifts.update', $shift->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <div class="card-header">
+                            <h4>Input Text</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" class="form-control" name="name" value="{{ $shift->name }}" required>
+                            </div>
+                            <div class="form-group">
+                                <label>Time In</label>
+                                <input type="time" class="form-control" name="time_in" value="{{ $shift->time_in }}" required>
+                            </div>
+                            <div class="form-group">
+                                <label>Time Out</label>
+                                <input type="time" class="form-control" name="time_out" value="{{ $shift->time_out }}" required>
+                            </div>
+
+                        </div>
+                        <div class="card-footer text-right">
+                            <button class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </section>
+    </div>
+@endsection
+
+@push('scripts')
+@endpush
