b6c0aa0b3e7b938cdbdc5b6ad38b05e24b88dd37
zainalsalamun <zainalrtf@gmail.com>
2026-02-20 10:12:53 +0700

    feat: Add Verifikasi Pensiun page with dedicated controller and views, implement CSV export for attendances and verifikasi, and update sidebar icons.

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -13,54 +13,54 @@
             </li>
 
             <li class="nav-item ">
-                <a href="{{ route('users.index') }}" class="nav-link "><i class="fas fa-columns"></i>
+                <a href="{{ route('users.index') }}" class="nav-link "><i class="fas fa-users"></i>
                     <span>Users</span></a>
             </li>
 
             <li class="nav-item">
                 <a href="{{ route('companies.show', 1) }}" class="nav-link">
-                    <i class="fas fa-columns"></i>
+                    <i class="fas fa-building"></i>
                     <span>Company</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a href="{{ route('attendances.index') }}" class="nav-link">
-                    <i class="fas fa-columns"></i>
+                    <i class="fas fa-calendar-check"></i>
                     <span>Attendances</span>
                 </a>
             </li>
 
             <li class="nav-item">
                 <a href="{{ route('permissions.index') }}" class="nav-link">
-                    <i class="fas fa-columns"></i>
+                    <i class="fas fa-file-alt"></i>
                     <span>Permission</span>
                 </a>
             </li>
 
             <li class="nav-item">
                 <a href="{{ route('reimbursements.index') }}" class="nav-link">
-                    <i class="fas fa-columns"></i>
+                    <i class="fas fa-money-bill-wave"></i>
                     <span>Reimbursement</span>
                 </a>
             </li>
 
             <li class="nav-item">
                 <a href="{{ route('shifts.index') }}" class="nav-link">
-                    <i class="fas fa-columns"></i>
+                    <i class="fas fa-clock"></i>
                     <span>Shift</span>
                 </a>
             </li>
 
             <li class="nav-item">
                 <a href="{{ route('qr_absens.index') }}" class="nav-link">
-                    <i class="fas fa-columns"></i>
+                    <i class="fas fa-qrcode"></i>
                     <span>QR Absen</span>
                 </a>
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('attendances.index') }}" class="nav-link">
-                    <i class="fas fa-columns"></i>
+                <a href="{{ route('verifikasi.index') }}" class="nav-link">
+                    <i class="fas fa-user-check"></i>
                     <span>Verifikasi Pensiun</span>
                 </a>
             </li>
--- resources/views/pages/verifikasi/index.blade.php
+++ resources/views/pages/verifikasi/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Attendances')
+@section('title', 'Verifikasi Pensiun')
 
 @push('style')
     <!-- CSS Libraries -->
@@ -11,14 +11,14 @@
     <div class="main-content">
         <section class="section">
             <div class="section-header">
-                <h1>Attendances</h1>
+                <h1>Verifikasi Pensiun</h1>
                 {{-- <div class="section-header-button">
-                    <a href="{{ route('attendances.create') }}" class="btn btn-primary">Add New</a>
+                    <a href="{{ route('verifikasi.create') }}" class="btn btn-primary">Add New</a>
                 </div> --}}
                 <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                    <div class="breadcrumb-item"><a href="#">Attendances</a></div>
-                    <div class="breadcrumb-item">All Attendances</div>
+                    <div class="breadcrumb-item"><a href="#">Verifikasi Pensiun</a></div>
+                    <div class="breadcrumb-item">All Verifikasi</div>
                 </div>
             </div>
             <div class="section-body">
@@ -27,9 +27,9 @@
                         @include('layouts.alert')
                     </div> --}}
                 </div>
-                <h2 class="section-title">Attendances</h2>
+                <h2 class="section-title">Verifikasi Pensiun</h2>
                 <p class="section-lead">
-                    You can manage all Attendances, such as editing, deleting and more.
+                    You can manage all Verifikasi Pensiun, such as editing, deleting and more.
                 </p>
 
 
@@ -42,7 +42,7 @@
                             <div class="card-body">
 
                                 <div class="float-right">
-                                    <form method="GET" action="{{ route('attendances.index') }}">
+                                    <form method="GET" action="{{ route('verifikasi.index') }}">
                                         <div class="input-group">
                                             <input type="text" class="form-control" placeholder="Search by name" name="name">
                                             <div class="input-group-append">
@@ -103,13 +103,13 @@
 
                                                 <td>
                                                     <div class="d-flex justify-content-center">
-                                                        <a href='{{ route('attendances.edit', $attendance->id) }}'
+                                                        <a href='{{ route('verifikasi.edit', $attendance->id) }}'
                                                             class="btn btn-sm btn-info btn-icon">
                                                             <i class="fas fa-edit"></i>
                                                             Edit
                                                         </a>
 
-                                                        <form action="{{ route('attendances.destroy', $attendance->id) }}"
+                                                        <form action="{{ route('verifikasi.destroy', $attendance->id) }}"
                                                             method="POST" class="ml-2">
                                                             <input type="hidden" name="_method" value="DELETE" />
                                                             <input type="hidden" name="_token"
@@ -128,7 +128,7 @@
                                 </div>
                                 <div class="float-left">
                                     <div class="card-header-action">
-                                        <a href="{{ route('attendances.export-csv', request()->query()) }}" class="btn btn-success">Download</a>
+                                        <a href="{{ route('verifikasi.export-csv', request()->query()) }}" class="btn btn-success">Download</a>
                                     </div>
                                 </div>
                                 <div class="float-right">
